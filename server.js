43f51902c06973fa3f08bf6b131203a40f391091
Javiera Fortunato <jfortunato@moveapps.cl>
2022-04-26 21:00:12 -0400

    Agrego app.use para cors

--- server.js
+++ server.js
@@ -21,11 +21,14 @@ const app = express();
 
 //Habilitar cors
 // app.use(cors(corsOptions));
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200,
-}
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+app.use(cors());
 
 //Habilitar req.body:
 app.use(express.json());
